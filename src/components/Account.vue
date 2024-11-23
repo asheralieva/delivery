192f761870337a06711b653136930be3fb3b1779
asheralieva <84007297+asheralieva@users.noreply.github.com>
2024-11-23 06:57:46 +0600

    account page api calls

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -4,20 +4,11 @@
       <div class="account-content">
         <div class="account-info">
           <img src="../assets/logo-profile.webp" alt="" />
-          <span>{{account.last_name }} {{ account.first_name  }}</span>
+          <span>{{ account.last_name }} {{ account.first_name }}</span>
           <span>{{ account.username }}</span>
         </div>
-        <div class="account-history">
-          <span>История заказов:</span>
-          <div class="history-item">
-            <span>
-              AAAaaaaaaaaaaaaaaaaa --<i class="arrow right"></i>
-              BBBbbbbbbbbbbbb</span
-            >
-            <span>размер: большой</span>
-            <span>статус: завершен</span>
-          </div>
-        </div>
+
+        <History />
       </div>
     </div>
   </section>
@@ -25,8 +16,11 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
+import History from "../components/History.vue"
+
 
 const account = ref({});
+const orders = ref({});
 
 onMounted(() => {
   const token = localStorage.getItem("accessToken"); // Замените на ваш токен
@@ -46,9 +40,30 @@ onMounted(() => {
     })
     .then((data) => {
       console.log(data);
-      account.value = data;  // Используем .value для изменения значения
-      
-      })
+      account.value = data; // Используем .value для изменения значения
+    })
+    .catch((error) => {
+      console.error("Ошибка при получении данных:", error);
+    });
+
+
+    fetch("http://127.0.0.1:8000/api/orders/user/", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`, // Добавляем токен
+      "Content-Type": "application/json", // Укажите, если требуется
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Ошибка HTTP: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      orders.value = data; // Используем .value для изменения значения
+    })
     .catch((error) => {
       console.error("Ошибка при получении данных:", error);
     });
@@ -83,36 +98,7 @@ onMounted(() => {
       margin-top: 20px;
     }
   }
-  &-history {
-    p {
-      color: #1852f0;
-    }
-    .history-item {
-      display: flex;
-      gap: 20px;
-      margin-top: 12px;
-      color: #1852f0;
-      box-shadow: 0 0 10px #1852f0;
-      border: 2px solid #1852f0;
-      padding: 8px;
-      border-radius: 20px;
-      background-color: #d2dcf9;
-      box-shadow: #000;
-      span {
-        i {
-          border: solid #1852f0;
-          border-width: 0 2px 2px 0;
-          display: inline-block;
-          padding: 3px;
-        }
-
-        .right {
-          transform: rotate(-45deg);
-          -webkit-transform: rotate(-45deg);
-        }
-      }
-    }
-  }
+ 
 }
 
 @media screen and (max-width: 768px) {
@@ -127,11 +113,7 @@ onMounted(() => {
 @media screen and (max-width: 425px) {
   .account {
     margin: 25px 50px;
-    &-history {
-      .history-item {
-        flex-wrap: wrap;
-      }
-    }
+    
   }
 }
 </style>
---
+++ src/components/History.vue
@@ -0,0 +1,81 @@
+<template>
+  <section class="history">
+    <div class="account-history">
+      <span>{{ title }}</span>
+      <div class="history-item">
+        <span>
+          {{ start }}
+          {{ end }}
+        </span>
+        <span>статус: {{ status }}</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'История заказов',
+  },
+  start: {
+    type: String,
+    default: 'AAAaaaaaaaaaaaaaaaaa',
+  },
+  end: {
+    type: String,
+    default: 'BBBbbbbbbbbbbbb',
+  },
+  size: {
+    type: String,
+    default: 'большой',
+  },
+  status: {
+    type: String,
+    default: 'завершен',
+  },
+})
+</script>
+
+<style lang="scss">
+.account-history {
+  p {
+    color: #1852f0;
+  }
+  .history-item {
+    display: flex;
+    gap: 20px;
+    margin-top: 12px;
+    color: #1852f0;
+    box-shadow: 0 0 10px #1852f0;
+    border: 2px solid #1852f0;
+    padding: 8px;
+    border-radius: 20px;
+    background-color: #d2dcf9;
+    box-shadow: #000;
+    span {
+      i {
+        border: solid #1852f0;
+        border-width: 0 2px 2px 0;
+        display: inline-block;
+        padding: 3px;
+      }
+
+      .right {
+        transform: rotate(-45deg);
+        -webkit-transform: rotate(-45deg);
+      }
+    }
+  }
+}
+@media screen and (max-width: 425px) {
+  .account-history {
+    .history-item {
+      flex-wrap: wrap;
+    }
+  }
+}
+</style>
